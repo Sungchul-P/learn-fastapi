b1cce5ebfd94107ac15090d8c97117c39c901f51
Sungchul Park <sungchul.dev@gmail.com>
2023-03-07 18:47:18 +0900

    feat: 게시글 CR 엔드포인트 구현

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # learn-fastapi
 FastAPI 사용법을 익히기 위한 레포지터리
 
-## 프로젝트 초기 설정
+## 초기 설정
 
 ```shell
 pyenv local 3.9
@@ -11,4 +11,13 @@ poetry shell
 
 poetry add fastapi
 poetry add "uvicorn[standard]"
+```
+
+## 실행 및 API 테스트
+
+```shell
+uvicorn main:app --reload
+
+# Swagger UI
+http://127.0.0.1:8000/docs
 ```
--- main.py
+++ main.py
@@ -1,8 +1,43 @@
-from fastapi import FastAPI
+from typing import Union
+
+from fastapi import FastAPI, HTTPException, status
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class Post(BaseModel):
+    id: int
+    title: str
+    content: Union[str, None] = None
+
+
+posts = []
+next_post_id = 1
+
+
+@app.post("/posts/")
+async def create_post(post: Post):
+    global next_post_id
+    post.id = next_post_id
+    next_post_id += 1
+    posts.append(post)
+    print(posts)
+    return post
+
+
+@app.get("/posts/{post_id}")
+async def read_post(post_id: int):
+    global posts
+    post = next((p for p in posts if p.id == post_id), None)
+    if post is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Post not found"
+        )
+    return post
+
+
+@app.get("/posts/")
+async def read_posts():
+    global posts
+    return {"posts": posts}
