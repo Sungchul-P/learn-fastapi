97de0adaa876aed74c9feb82354f0dfdce41d1af
Sungchul Park <sungchul.dev@gmail.com>
2023-03-16 21:23:55 +0900

    feat: 게시글 UD 엔드포인트 구현

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Optional, List
 
 from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel
@@ -7,22 +7,32 @@ app = FastAPI()
 
 
 class Post(BaseModel):
-    id: int
+    id: Optional[int]
     title: str
-    content: Union[str, None] = None
+    content: Optional[str]
+    author: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "title": "FastAPI Tutorial",
+                "content": "Learn how to build APIs with FastAPI and Python.",
+                "author": "beginner"
+            }
+        }
+        exclude = {'id'}
 
 
 posts = []
 next_post_id = 1
 
 
-@app.post("/posts/")
+@app.post("/posts/", status_code=status.HTTP_201_CREATED)
 async def create_post(post: Post):
     global next_post_id
     post.id = next_post_id
     next_post_id += 1
     posts.append(post)
-    print(posts)
     return post
 
 
@@ -30,9 +40,9 @@ async def create_post(post: Post):
 async def read_post(post_id: int):
     global posts
     post = next((p for p in posts if p.id == post_id), None)
-    if post is None:
+    if not post:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Post not found"
+            status_code=status.HTTP_404_NOT_FOUND, detail="페이지를 찾을 수 없습니다"
         )
     return post
 
@@ -41,3 +51,39 @@ async def read_post(post_id: int):
 async def read_posts():
     global posts
     return {"posts": posts}
+
+
+@app.put("/posts/{post_id}")
+async def update_post(post_id: int, new_post: Post):
+    global posts
+    post = next((p for p in posts if p.id == post_id), None)
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="페이지를 찾을 수 없습니다"
+        )
+
+    if post.author != new_post.author:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="게시글 작성자만 수정할 수 있습니다"
+        )
+
+    post.title = new_post.title
+    post.content = new_post.content
+
+    return post
+
+
+@app.delete("/posts/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_post(post_id: int, author: str):
+    global posts
+    for index, post in enumerate(posts):
+        if post.id == post_id:
+            if post.author != author:
+                raise HTTPException(
+                    status_code=status.HTTP_403_FORBIDDEN, detail="게시글 작성자만 삭제할 수 있습니다"
+                )
+            del posts[index]
+            return
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail="페이지를 찾을 수 없습니다"
+    )
