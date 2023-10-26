162408595755fc12b27dacef44cb52b7f0b5bcaf
Sungchul Park <48536668+Sungchul-P@users.noreply.github.com>
2023-10-26 18:44:15 +0900

    refactor: API 엔드포인트 반환 값 타입힌트 추가 (#12)

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Any, List, Optional
 
 from fastapi import APIRouter, Depends, Query
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
@@ -7,6 +7,7 @@ from starlette import status
 
 from database import engine
 from exceptions import NotAuthenticated
+from model import Comment, Post, User
 from service import (
     CommentCreate,
     CommentRead,
@@ -49,7 +50,7 @@ def get_session():
 
 def get_current_session(
     credentials: HTTPBasicCredentials = Depends(security), session: Session = Depends(get_session)
-):
+) -> Optional[Any]:
     username = credentials.username
     user, user_session = get_current_user(username, session)
     if not user or not user_session:
@@ -61,12 +62,12 @@ def get_current_session(
 @router.post("/users/login")
 async def login_route(
     credentials: HTTPBasicCredentials = Depends(security), session: Session = Depends(get_session)
-):
+) -> dict[str, str]:
     return await login(credentials, session)
 
 
 @router.post("/users/logout")
-async def logout_route(current_session=Depends(get_current_session)):
+async def logout_route(current_session=Depends(get_current_session)) -> dict[str, str]:
     current_user_id = current_session["user_id"]
 
     return await logout(current_user_id)
@@ -77,108 +78,118 @@ async def create_user_route(user: UserCreate, session: Session = Depends(get_ses
     return await create_user(user, session)
 
 
-@router.get("/users/")
+@router.get("/users/", status_code=status.HTTP_200_OK)
 async def read_users_route(
     offset: int = 0, limit: int = Query(default=10), session: Session = Depends(get_session)
-):
+) -> List[User]:
     return await read_users(offset, limit, session)
 
 
-@router.get("/users/{user_id}", response_model=UserRead)
-async def read_user_route(user_id: str, session: Session = Depends(get_session)):
+@router.get("/users/{user_id}", status_code=status.HTTP_200_OK, response_model=UserRead)
+async def read_user_route(user_id: str, session: Session = Depends(get_session)) -> User:
     return await read_user(user_id, session)
 
 
-@router.get("/users/{user_id}/posts", response_model=List[PostRead])
+@router.get(
+    "/users/{user_id}/posts", status_code=status.HTTP_200_OK, response_model=List[PostRead]
+)
 async def read_user_posts_route(
     user_id: str,
     page: int = 0,
     limit: int = Query(default=5),
     session: Session = Depends(get_session),
-):
+) -> List[Post]:
     offset = page * limit
     return await read_user_posts(user_id, offset, limit, session)
 
 
-@router.get("/users/{user_id}/comments", response_model=List[CommentRead])
+@router.get(
+    "/users/{user_id}/comments", status_code=status.HTTP_200_OK, response_model=List[CommentRead]
+)
 async def read_user_comments_route(
     user_id: str,
     page: int = 0,
     limit: int = Query(default=5),
     session: Session = Depends(get_session),
-):
+) -> list[Comment]:
     offset = page * limit
     return await read_user_comments(user_id, offset, limit, session)
 
 
-@router.put("/users/{user_id}")
+@router.put("/users/{user_id}", status_code=status.HTTP_200_OK)
 async def update_user_route(
     user_id: str, user: UserUpdate, session: Session = Depends(get_session)
-):
+) -> User:
     return await update_user(user_id, user, session)
 
 
-@router.delete("/users/{user_id}")
-async def delete_user_route(user_id: str, password: str, session: Session = Depends(get_session)):
+@router.delete("/users/{user_id}", status_code=status.HTTP_200_OK)
+async def delete_user_route(
+    user_id: str, password: str, session: Session = Depends(get_session)
+) -> dict[str, bool]:
     return await delete_user(user_id, password, session)
 
 
 @router.post("/posts/", status_code=status.HTTP_201_CREATED)
-async def create_post_route(post: PostCreate, session: Session = Depends(get_session)):
+async def create_post_route(post: PostCreate, session: Session = Depends(get_session)) -> Post:
     return await create_post(post, session)
 
 
-@router.get("/posts/")
+@router.get("/posts/", status_code=status.HTTP_200_OK)
 async def read_posts_route(
     offset: int = 0, limit: int = Query(default=100), session: Session = Depends(get_session)
-):
+) -> List[Post]:
     return await read_posts(offset, limit, session)
 
 
-@router.get("/posts/{post_id}")
-async def read_post_route(post_id: int, session: Session = Depends(get_session)):
+@router.get("/posts/{post_id}", status_code=status.HTTP_200_OK)
+async def read_post_route(post_id: int, session: Session = Depends(get_session)) -> Post:
     return await read_post(post_id, session)
 
 
-@router.put("/posts/{post_id}")
+@router.put("/posts/{post_id}", status_code=status.HTTP_200_OK)
 async def update_post_route(
     post_id: int, post: PostUpdate, session: Session = Depends(get_session)
-):
+) -> Post:
     return await update_post(post_id, post, session)
 
 
-@router.delete("/posts/{post_id}")
-async def delete_post_route(post_id: int, author: str, session: Session = Depends(get_session)):
+@router.delete("/posts/{post_id}", status_code=status.HTTP_200_OK)
+async def delete_post_route(
+    post_id: int, author: str, session: Session = Depends(get_session)
+) -> dict[str, bool]:
     return await delete_post(post_id, author, session)
 
 
 @router.post("/posts/{post_id}/comments/", status_code=status.HTTP_201_CREATED)
 async def create_comment_route(
     post_id: int, comment: CommentCreate, session: Session = Depends(get_session)
-):
+) -> Comment:
     return await create_comment(post_id, comment, session)
 
 
-@router.get("/posts/{post_id}/comments/", response_model=List[CommentRead])
+@router.get(
+    "/posts/{post_id}/comments/", status_code=status.HTTP_200_OK, response_model=List[CommentRead]
+)
 async def read_post_comments_route(
     post_id: int,
     page: int = 0,
     limit: int = Query(default=5),
     session: Session = Depends(get_session),
-):
+) -> List[Comment]:
     offset = page * limit
     return await read_post_comments(post_id, offset, limit, session)
 
 
-@router.put("/posts/{post_id}/comments/{comment_id}")
+@router.put("/posts/{post_id}/comments/{comment_id}", status_code=status.HTTP_200_OK)
 async def update_comment_route(
     post_id: int, comment_id: int, comment: CommentUpdate, session: Session = Depends(get_session)
-):
+) -> Comment:
     return await update_comment(post_id, comment_id, comment, session)
 
 
-@router.delete("/posts/{post_id}/comments/{comment_id}")
+@router.delete("/posts/{post_id}/comments/{comment_id}", status_code=status.HTTP_200_OK)
 async def delete_comment_route(
     comment_id: int, author: str, session: Session = Depends(get_session)
-):
+) -> dict[str, bool]:
     return await delete_comment(comment_id, author, session)
--- service.py
+++ service.py
@@ -140,7 +140,7 @@ async def get_comments_by_post(
     return comment
 
 
-async def create_user(user: UserCreate, session: Session):
+async def create_user(user: UserCreate, session: Session) -> User:
     try:
         db_user = User.from_orm(user)
         session.add(db_user)
@@ -152,27 +152,29 @@ async def create_user(user: UserCreate, session: Session):
     return db_user
 
 
-async def read_users(offset: int, limit: int, session: Session):
+async def read_users(offset: int, limit: int, session: Session) -> List[User]:
     users = session.exec(select(User).offset(offset).limit(limit)).all()
     return users
 
 
-async def read_user(user_id: str, session: Session):
+async def read_user(user_id: str, session: Session) -> User:
     user = get_user_by_id(user_id, session)
     if not user:
         raise UserNotFoundException
     return user
 
 
-async def read_user_posts(user_id: str, offset: int, limit: int, session: Session):
+async def read_user_posts(user_id: str, offset: int, limit: int, session: Session) -> List[Post]:
     return await get_posts_by_user(user_id, offset, limit, session)
 
 
-async def read_user_comments(user_id: str, offset: int, limit: int, session: Session):
+async def read_user_comments(
+    user_id: str, offset: int, limit: int, session: Session
+) -> List[Comment]:
     return await get_comments_by_user(user_id, offset, limit, session)
 
 
-async def update_user(user_id: str, user: UserUpdate, session: Session):
+async def update_user(user_id: str, user: UserUpdate, session: Session) -> User:
     db_user: Optional[User] = get_user_by_id(user_id, session)
     if not db_user:
         raise UserNotFoundException
@@ -188,7 +190,7 @@ async def update_user(user_id: str, user: UserUpdate, session: Session):
     return db_user
 
 
-async def delete_user(user_id: str, password: str, session: Session):
+async def delete_user(user_id: str, password: str, session: Session) -> dict[str, bool]:
     user = get_user_by_id(user_id, session)
     if not user:
         raise UserNotFoundException
@@ -204,7 +206,7 @@ def get_post_by_id(post_id: int, session: Session) -> Optional[Post]:
     return session.get(Post, post_id)
 
 
-async def create_post(post: PostCreate, session: Session):
+async def create_post(post: PostCreate, session: Session) -> Post:
     try:
         db_post = Post.from_orm(post)
         session.add(db_post)
@@ -216,19 +218,19 @@ async def create_post(post: PostCreate, session: Session):
     return db_post
 
 
-async def read_posts(offset: int, limit: int, session: Session):
+async def read_posts(offset: int, limit: int, session: Session) -> List[Post]:
     posts = session.exec(select(Post).offset(offset).limit(limit)).all()
     return posts
 
 
-async def read_post(post_id: int, session: Session):
+async def read_post(post_id: int, session: Session) -> Post:
     post = get_post_by_id(post_id, session)
     if not post:
         raise PostNotFoundException(post_id)
     return post
 
 
-async def update_post(post_id: int, post: PostUpdate, session: Session):
+async def update_post(post_id: int, post: PostUpdate, session: Session) -> Post:
     db_post: Optional[Post] = get_post_by_id(post_id, session)
     if not db_post:
         raise PostNotFoundException(post_id)
@@ -244,7 +246,7 @@ async def update_post(post_id: int, post: PostUpdate, session: Session):
     return db_post
 
 
-async def delete_post(post_id: int, author_id: str, session: Session):
+async def delete_post(post_id: int, author_id: str, session: Session) -> dict[str, bool]:
     post = get_post_by_id(post_id, session)
     if not post:
         raise PostNotFoundException(post_id)
@@ -256,7 +258,7 @@ async def delete_post(post_id: int, author_id: str, session: Session):
     return {"ok": True}
 
 
-async def create_comment(post_id: int, comment: CommentCreate, session: Session):
+async def create_comment(post_id: int, comment: CommentCreate, session: Session) -> Comment:
     try:
         db_comment = Comment(post_id=post_id, **comment.dict())
         db_comment.post_id = post_id
@@ -269,11 +271,15 @@ async def create_comment(post_id: int, comment: CommentCreate, session: Session)
     return db_comment
 
 
-async def read_post_comments(post_id: int, offset: int, limit: int, session: Session):
+async def read_post_comments(
+    post_id: int, offset: int, limit: int, session: Session
+) -> List[Comment]:
     return await get_comments_by_post(post_id, offset, limit, session)
 
 
-async def update_comment(post_id: int, comment_id: int, comment: CommentUpdate, session: Session):
+async def update_comment(
+    post_id: int, comment_id: int, comment: CommentUpdate, session: Session
+) -> Comment:
     db_comment: Optional[Comment] = session.get(Comment, comment_id)
     if not db_comment:
         raise CommentNotFoundException(comment_id)
@@ -291,7 +297,7 @@ async def update_comment(post_id: int, comment_id: int, comment: CommentUpdate, 
     return db_comment
 
 
-async def delete_comment(comment_id: int, author_id: str, session: Session):
+async def delete_comment(comment_id: int, author_id: str, session: Session) -> dict[str, bool]:
     comment: Optional[Comment] = session.get(Comment, comment_id)
     if not comment:
         raise CommentNotFoundException(comment_id)
@@ -306,7 +312,7 @@ async def delete_comment(comment_id: int, author_id: str, session: Session):
 user_sessions: Dict[str, Any] = {}
 
 
-def get_current_user(username: str, session: Session):
+def get_current_user(username: str, session: Session) -> tuple[User, Optional[Any]]:
     user_session = user_sessions.get(username)
     if user_session and user_session["expire_date"] > datetime.now():
         user: Optional[User] = get_user_by_id(username, session)
@@ -316,7 +322,7 @@ def get_current_user(username: str, session: Session):
     raise NotAuthenticated
 
 
-async def login(credentials: HTTPBasicCredentials, session: Session):
+async def login(credentials: HTTPBasicCredentials, session: Session) -> dict[str, str]:
     user: Optional[User] = await read_user(credentials.username, session)
 
     if not user or user.password != credentials.password:
@@ -335,7 +341,7 @@ async def login(credentials: HTTPBasicCredentials, session: Session):
     return {"message": f"{user.id} 로그인 성공!"}
 
 
-async def logout(user_id: str):
+async def logout(user_id: str) -> dict[str, str]:
     user_session = user_sessions.get(user_id)
     if user_session:
         del user_sessions[user_id]
